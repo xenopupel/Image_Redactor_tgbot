869cf924173d5828bd45230ea3632facc4ca414b
pk <ivannabsid@mail.ru>
2021-06-10 19:59:46 +0300

    initial commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,8 +7,6 @@ import java.util.Properties;
 
 public class App {
     public static void main(String[] args) throws IOException, TelegramApiException {
-        Properties tgProps= new Properties();
-        tgProps.load(App.class.getClassLoader().getResourceAsStream("tg.properties"));
         final Bot bot = new Bot("ITMEMEBOT", "1874564989:AAFn2bny0ML7QgvwVPfPcETkS6h2dnuB3Dk");
 
         TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
